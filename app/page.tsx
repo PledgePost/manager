2ab49a8d3f4b637a56db6983ceb2ddf4c7d9d4e8
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-12-06 17:39:28 +0900

    fetchRound

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,135 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { showSuccessToast } from "@/hooks/useNotification";
-
-export default function Home() {
-  function handleClick() {
-    showSuccessToast("https://github.com/tnkshuuhei");
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
+interface Round {
+  id: string;
+  owner: string;
+  ipoolAddress: string;
+  name: string;
+  startDate: string;
+  endDate: string;
+  articles: any;
+}
+async function getRound() {
+  try {
+    const res = await fetch(
+      "https://api.studio.thegraph.com/query/52298/pledgepost_mumbai/version/latest",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query: `
+					query GetAllRounds($id: String!) {
+						rounds(where: {id: $id}) {	
+							id
+							owner
+							ipoolAddress
+							name
+							startDate
+							endDate
+							articles {
+								id
+								content
+								articleId
+								authorAddress
+							}
+						}
+					}
+					`,
+          variables: { id: "1" },
+        }),
+      }
+    ).then((res) => res.json());
+    return res.data?.rounds;
+  } catch (e) {
+    console.error(e);
   }
+}
+
+async function getAllRound() {
+  try {
+    const res = await fetch(
+      "https://api.studio.thegraph.com/query/52298/pledgepost_mumbai/version/latest",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query: `
+					{
+						rounds {
+							id
+							owner
+							ipoolAddress
+							name
+							startDate
+							endDate
+							articles {
+								id
+								content
+								articleId
+								authorAddress
+							}
+						}
+					}
+					`,
+        }),
+      }
+    ).then((res) => res.json());
+    return res.data?.rounds;
+  } catch (e) {
+    console.error(e);
+  }
+}
+export default async function Home() {
+  const rounds: Round[] = await getAllRound();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button onClick={handleClick}>Click me</Button>
+      {rounds.length === 0 ? (
+        <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
+          No round available
+        </p>
+      ) : (
+        <>
+          {rounds.map((round: any, index: number) => (
+            <div
+              key={index}
+              className="rounded-[15px] bg-white cursor-pointer shadow-lg"
+            >
+              <div className="w-[400px] ">
+                <Image
+                  src={"/favicon.ico"}
+                  alt="roundImage"
+                  width={400}
+                  height={200}
+                  priority
+                />
+              </div>
+              <div className="flex flex-col p-4">
+                <div className="flex items-center mt-[10px] gap-[12px]">
+                  <Avatar>
+                    <AvatarImage src="/favicon.ico" />
+                    <AvatarFallback></AvatarFallback>
+                  </Avatar>
+                  <h3 className="font-epilogue font-semibold text-[16px] text-black text-left leading-[26px] truncate">
+                    {round.name}
+                  </h3>
+                </div>
+                <div className="flex justify-between flex-wrap mt-[10px] gap-2">
+                  <div className="flex flex-col">
+                    <h4 className="font-epilogue font-semibold text-[14px] text-[#808191] leading-[22px]">
+                      Start: {round.startDate} - End: {round.endDate}
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </main>
   );
 }
