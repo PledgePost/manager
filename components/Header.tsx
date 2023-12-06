005a56c774cb4ac18a36bf322fedccffcd5462de
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-12-06 22:12:22 +0900

    import components from shadcn

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
                 <Link
-                  href="/"
+                  href="/rounds"
                   className="block py-2 pr-4 pl-3 text-gray-700 bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0"
                   aria-current="page"
                 >
@@ -31,10 +31,10 @@ const Header = () => {
               </li>
               <li>
                 <Link
-                  href="/user"
+                  href="/create"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
                 >
-                  Account
+                  Create
                 </Link>
               </li>
               <li>
